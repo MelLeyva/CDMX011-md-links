a4ccadf8c105ce1e6e7032ae9c21d61c77c090a7
MelLeyva <mleyva0795@gmail.com>
2021-10-13 01:04:52 -0500

    function type-route modularized

--- Lib/type-route.js
+++ Lib/type-route.js
@@ -1,11 +1,13 @@
 const path = require('path');
-const rout = process.argv[2]
 
-const typeRoute = path.isAbsolute(rout)
-
-if (typeRoute === false ){
-   const newPath =  path.resolve(rout)
-   console.log(newPath)
-} else {
-    console.log(typeRoute)
+function typeRoute (route) {
+    const isAbsolute = path.isAbsolute(route)
+    if (isAbsolute === false ){
+       const changeRoute =  path.resolve(route)
+       return changeRoute
+    } else {
+        return route
+    }
 }
+
+module.exports.typeRoute = typeRoute
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-module.exports = () => {
-  // ...
-};
+const getRoute = require('./Lib/type-route.js')
+const route = process.argv[2]
+
+
+console.log(getRoute.typeRoute(route))
